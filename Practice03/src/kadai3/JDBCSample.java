1da8868073751e15f4a24bf4b9ee8bc2776d700e
y_jiang <y_jiang@banananoair>
2023-03-09 16:10:37 +0900

    課題3完成です。

--- Practice03/src/kadai3/JDBCSample.java
+++ Practice03/src/kadai3/JDBCSample.java
@@ -3,17 +3,16 @@ package kadai3;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.security.Timestamp;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Random;
 
 public class JDBCSample {
 	public static void main(String[] args) throws Exception {
@@ -22,168 +21,162 @@ public class JDBCSample {
 		final String URL = "jdbc:postgresql://127.0.0.1:5432/postgres";
 		final String USER = null;
 		final String PASS = null;
-		final String SQL1 = "select * from unseimaster order by unseicd asc;";
 
-		final String SQL2 = "select * from omikuji where omikujicd = ? And unseicd = ? And negaigoto = ? And akinai = ? And gakumon = ? And modifiedby = ? And updatedate = ? And createdby = ? And  createddate = ?;";
-//	        final String SQL = "select * from unseimaster where unseiname = ?;";        
+		final String SQL1 = "select * from omikuji order by omikujicd asc;";
+		final String SQL2 = "insert into omikuji (omikujicd,unseicd,negaigoto,akinai,gakumon,modifiedby,updatedate,createdby,createddate)values(nextval('omikujicd_seq'),?,?,?,?,?,CURRENT_TIMESTAMP,?,CURRENT_TIMESTAMP);";
+		final String SQL3 = "select * from result order by uranaidate asc;";
+		final String SQL4 = "insert into result (uranaidate,birthday,omikujicd,modifiedby,updatedate,createdby,createddate)values(?,?,?,?,CURRENT_TIMESTAMP,?,CURRENT_TIMESTAMP);";
+		final String SQL5 = "select * from omikuji where omikujicd = ?;";
 		Connection con = null;
 
-		///////////////////// TB読み込む//////////////////////////////
-		HashMap<String, String> map = new HashMap<String, String>();
-
-		List<Omikuji> list1 = new ArrayList<Omikuji>();
-		Omikuji omikuji = null;
-
-		File file = new File("/Users/y_jiang/Documents/unsei.csv");
+		// CSVファイル
+		File file = new File("/Users/y_jiang/Documents/unseiNew.csv");
 		FileReader fr = null;
+		con = DriverManager.getConnection(URL, USER, PASS);
 
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-		Date d = new Date();
-		java.util.Date  t=new java.util.Date();
-		java.sql.Date  t1=new java.sql.Date(t.getTime());
+		Omikuji omikuji = null;
+		try {
+			// omikujiテーブルにデータがすでに存在した場合、データの書き込みをスキップ。
+			PreparedStatement findOmkjInfo = con.prepareStatement(SQL1);
+			ResultSet omkjInfoRs = findOmkjInfo.executeQuery();
 
-		
-		//Timestamp t= new Timestamp(d.getTime());
-
-			con = DriverManager.getConnection(URL, USER, PASS);
-			PreparedStatement ps1 = con.prepareStatement(SQL1);
-			PreparedStatement ps2 = con.prepareStatement(SQL2);
-
-//	            ps.setString(1, "1");
-//	            ps.setString(1,"大吉");
-
-			ResultSet rs1 = null;
-			rs1 = ps1.executeQuery();
-
-//			ResultSet rs2 = null;
-//			rs2 = ps2.executeQuery();
-
-			while (rs1.next()) {
-//	                System.out.println(
-//	            		
-//	                   rs.getInt("unseicd") + " " +
-//	                   rs.getString("unseiname"));
-				int i = rs1.getInt("unseicd");
-				String unseicd = Integer.toString(i);
-				String unseiname = rs1.getString("unseiname");
-
-				// 定义好key和value，由于数据较多，后面还需筛选
-				map.put(unseiname, unseicd);
-
+			// 件数
+			int count = 0;
+			// 件数取得
+			while (omkjInfoRs.next()) {
+				count = omkjInfoRs.getRow();
 			}
-			// System.out.println(map.get("末吉"));
 
-			fr = new FileReader(file);
-			BufferedReader in = new BufferedReader(fr);
+			if (count <= 0) {// nullの場合、insert実行
+				PreparedStatement insertOmkjInfo = con.prepareStatement(SQL2);
+				fr = new FileReader(file);
+				BufferedReader in = new BufferedReader(fr);
+				String data;
+				int i = 0;
+				// CSVファイルを読み込み、DBに書き込み
+				while ((data = in.readLine()) != null) {
+					String[] arr = data.split(",");
+					// 運勢コード
+					insertOmkjInfo.setString(1, arr[0]);
+					// 願い事
+					insertOmkjInfo.setString(2, arr[1]);
+					// 商い
+					insertOmkjInfo.setString(3, arr[2]);
+					// 学問
+					insertOmkjInfo.setString(4, arr[3]);
+					// 更新者
+					insertOmkjInfo.setString(5, "姜悦");
+					// 作成者
+					insertOmkjInfo.setString(6, "姜悦");
+					// SQL実行
+					int insertCount = insertOmkjInfo.executeUpdate();
+					count = insertCount + i;
+					i++;
+				}
+				fr.close();
+			}
 
-			String date;
-			while ((date = in.readLine()) != null) {
+			// ****************ここは日付入力****************
+			System.out.println("誕生日を入力してください。");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String key = br.readLine();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+			// フォーマット設定
+			sdf.setLenient(false);
+			sdf.parse(key);
 
-				String[] arr = date.split(",");
+			// データ有無判定
+			boolean resultFlg = false;
+			// おみくじコード
+			String omikujicd = null;
 
-				switch (arr[0]) {
-				case "大吉":
+			Date today = new Date();
+			// resultテーブルに同じデータが存在するかどうかを判定
+			PreparedStatement findResultInfo = con.prepareStatement(SQL3);
+			ResultSet resultRs = findResultInfo.executeQuery();
+			while (resultRs.next()) {
+				// 同じ日かつ入力した誕生日が同じの場合はtrue
+				if (sdf.format(resultRs.getDate("uranaidate")).equals(sdf.format(today))) {
+					if (sdf.format(resultRs.getDate("birthday")).equals(key)) {
+						resultFlg = true;
+						// おみくじコード取得
+						omikujicd = resultRs.getString("omikujicd");
+					}
+				}
+			}
+
+			// 同じデータ存在しない場合insert
+			if (!resultFlg) {
+				PreparedStatement insertResultInfo = con.prepareStatement(SQL4);
+				Random random = new Random();
+
+				// 誕生日をDate型に
+				SimpleDateFormat sdFormat1 = new SimpleDateFormat("yyyyMMdd");
+				Date birth = sdFormat1.parse(key);
+				long newBirth = birth.getTime();
+
+				// 日付型変換
+				long day = today.getTime();
+				java.sql.Date uranaidate = new java.sql.Date(day);
+				java.sql.Date birthday = new java.sql.Date(newBirth);
+
+				// ランダムomikujicd生成
+				omikujicd = Integer.toString(random.nextInt(count));
+
+				insertResultInfo.setDate(1, uranaidate);
+				insertResultInfo.setDate(2, birthday);
+				insertResultInfo.setString(3, omikujicd);
+				insertResultInfo.setString(4, "姜悦");
+				insertResultInfo.setString(5, "姜悦");
+				insertResultInfo.executeUpdate();
+			}
+
+			// omikujicdで結果を検索
+			PreparedStatement resultInfo = con.prepareStatement(SQL5);
+			resultInfo.setString(1, omikujicd);
+			ResultSet getResultRs = resultInfo.executeQuery();
+			while (getResultRs.next()) {
+				switch (getResultRs.getString("unseicd")) {
+				case "1":
 					omikuji = new Daikichi();
 					break;
-				case "中吉":
+				case "2":
 					omikuji = new Chukichi();
 					break;
-				case "小吉":
+				case "3":
 					omikuji = new Shokichi();
 					break;
-				case "末吉":
+				case "4":
 					omikuji = new Suekichi();
 					break;
-				case "吉":
+				case "5":
 					omikuji = new Kichi();
 					break;
-				case "凶":
+				case "6":
 					omikuji = new Kyo();
 					break;
 
-				
-
+				}
 				omikuji.setUnsei();
-				// 前面筛选了吉凶，直接把结果套在map上就可以定义运势code
-				omikuji.setUnseicd(map.get(arr[0]));
-				omikuji.setNegaigoto(arr[1]);
-				omikuji.setAkinai(arr[2]);
-				omikuji.setGakumon(arr[3]);
+				omikuji.setOmikujicd(getResultRs.getString("omikujicd"));
+				omikuji.setUnseicd(getResultRs.getString("unseicd"));
+				omikuji.setNegaigoto(getResultRs.getString("negaigoto"));
+				omikuji.setAkinai(getResultRs.getString("akinai"));
+				omikuji.setGakumon(getResultRs.getString("gakumon"));
 
-				list1.add(omikuji);
-
-//				 System.out.println(omikuji.getUnsei() + " " + omikuji.getUnseicd() +
-//				  omikuji.getNegaigoto() + omikuji.getAkinai() +
-//				 omikuji.getGakumon());
-
-//		         
-//		            		
-//		                   rs.getInt("unseicd") + " " +
-//		                   rs.getString("unseiname"))
-
-//					int a = rs2.getInt("omikujicd");
-//					String omikujicd = Integer.toString(a);
-//					int b = rs2.getInt("unseicd");
-//					String unseicd = Integer.toString(b);
-//					String unseiname = rs2.getString("negaigoto");
-//					String unseiname = rs2.getString("akinai");
-//					String unseiname = rs2.getString("gakumon");
-//					
-			
-
-				
-
-				// 设置好omikujicodo
-				for (int i = 0; i < 50; i++) {
-					// 依次填入9个空
-					ps2.setInt(1, i + 1);
-					ps2.setString(2, omikuji.getUnseicd());
-					ps2.setString(3, omikuji.getNegaigoto());
-					ps2.setString(4, omikuji.getAkinai());
-					ps2.setString(5, omikuji.getGakumon());
-					ps2.setString(6, "姜悦");
-					ps2.setDate(7, t1);
-					ps2.setString(8, "姜悦");
-					ps2.setString(9, "20220704");
-				}
-
-					
-					
-					
-					
-					
-					
-			
-//					void	setString(int a, String b)	a番目の「?」にbをString型でセットする
-//					void	setInt(int a, int b)	a番目の「?」にbをint型でセットする
-//					void	setLong(int a, long b)	a番目の「?」にbをlong型でセットする
-//					void	setFloat(int a, float b)	a番目の「?」にbをfloat型でセットする
-//					void	setDouble(int a, double b)	a番目の「?」にbをdouble型でセットする
-//					void	setDate(int a, Date b)	a番目の「?」にbをjava.sql.Date型でセットする
-//					void	setBoolean(int a, boolean b)	a番目の「?」にbをboolean型でセットする
-//					void	clearParameters()	全てのパラメータをクリアする
-//					ResultSet	executeQuery()	SELECT文を実行する
-//					int	executeUpdate()	UPDATE, INSERT, DELETE,ストアドを実行する
-
-					 int a = ps2.executeUpdate();
-					 System.out.println("結果：" + a);
-
-				}
-			
-	
-				
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				con.close();
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
-		
+			// 結果出力
+			String result = omikuji.disp() + "," + omikuji.getNegaigoto() + "," + omikuji.getAkinai() + ","
+					+ omikuji.getGakumon();
+			System.out.println(result.replace(",", "\n"));
+
+		} catch (ParseException e) {
+			System.out.println("入力された日付は存在しません。");
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
+
 	}
+
 }
